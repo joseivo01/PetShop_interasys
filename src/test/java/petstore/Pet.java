324611aaf1352bcd9bc218b68658d459c9725465
joseivo01 <jose.lima@dcx.ufpb.br>
2021-09-03 10:36:21 -0300

    second commit here - addpet funtion test

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,2 +1,37 @@
-package petstore;public class Pet {
+package petstore;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class Pet {
+    String uri = "https://petstore.swagger.io/v2/pet";
+
+    public String readJson(String jsonPath) throws IOException{
+        return new String(Files.readAllBytes(Paths.get(jsonPath)));
+    }
+
+    @Test
+    public void addPet() throws IOException {
+        String jsonBody = readJson("db/pet1.json");
+
+        // Sintaxe gherkin
+        //Dado - Quando - Então
+        // Given - When - Then
+
+        given() //Dado
+                .contentType("application/json") //comum em API REST - antifas era "text/xml"
+                .log().all()
+                .body(jsonBody)
+        .when() //Quando
+                .post(uri)
+        .then() //Então
+                .log().all()
+                .statusCode(200)
+        ;
+    }
 }
